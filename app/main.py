66750add0c94038aae601fc0351c1e1d24425992
Hasan Maki <hasanmaki.ahmad@gmail.com>
2025-08-17 19:22:31 +0700

    feat: add logging middleware for request tracking
    implement LoggingMiddleware to log incoming requests and outgoing responses, including processing time and client information. This enhances observability and debugging capabilities in the FastAPI application.

--- app/main.py
+++ app/main.py
@@ -2,8 +2,15 @@ import uvicorn
 from fastapi import FastAPI
 
 from app.custom.cst_lifespan import app_lifespan
+from app.custom.cst_middleware import LoggingMiddleware
 
 app = FastAPI(lifespan=app_lifespan)
+app.add_middleware(LoggingMiddleware)
+
+
+@app.get("/")
+async def read_root():
+    return {"message": "Hello World"}
 
 
 if __name__ == "__main__":
