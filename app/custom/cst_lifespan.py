9942cb6c3342d6966839c0aad26d2904c02a05a7
Hasan Maki <hasanmaki.ahmad@gmail.com>
2025-08-19 06:38:22 +0700

    refactor(app lifespan): simplify lifespan context manager
    Streamline the app lifespan context manager by removing unnecessary repository initialization and error handling. This change enhances readability and maintains essential logging functionality during application startup and shutdown.

--- app/custom/cst_lifespan.py
+++ app/custom/cst_lifespan.py
@@ -2,39 +2,15 @@ from fastapi.concurrency import asynccontextmanager
 
 from app.config import get_settings
 from app.mlogg.setup import init_logging, logger
-from app.repositories import (
-    AsyncInMemoryMemberRepo,
-    AsyncInMemoryModuleRepo,
-    UserRepository,
-)
 
 ENV = get_settings().APP_ENV.value
 
 
 @asynccontextmanager
-async def app_lifespan(app):  # noqa: ANN001, RUF029
+async def app_lifespan(app):  # noqa: ANN001, ARG001, RUF029
     """Lifespan context manager for the FastAPI application."""
-    try:
-        init_logging()
-        logger.info("Application starting up")
-        # Initialize repositories and attach to app.state
-        with logger.contextualize(repo="member_repo"):
-            app.state.member_repo = AsyncInMemoryMemberRepo()
-            logger.info("Member repository initialized")
-        with logger.contextualize(repo="module_repo"):
-            app.state.module_repo = AsyncInMemoryModuleRepo()
-            logger.info("Module repository initialized")
-        with logger.contextualize(repo="auth_repo"):
-            app.state.auth_repo = UserRepository()
-            logger.info("User repository initialized")
-    except Exception as e:
-        logger.error(f"Error during application startup: {e}")
-        raise
+    init_logging()
+    logger.info("Application starting up")
     yield
     # cleanup
-    try:
-        app.state.member_repo = None
-        app.state.module_repo = None
-    except Exception as e:
-        logger.error(f"Error during application shutdown: {e}")
-        raise
+    logger.error("Error during application shutdown:")
