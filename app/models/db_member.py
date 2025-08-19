5844fc2108c0a658ba715e14ff95ce19ea7c9caa
Hasan Maki <hasanmaki.ahmad@gmail.com>
2025-08-19 12:17:44 +0700

    feat(models): update member and user models with audit fields
    Add audit fields and methods to the Member and User models for better tracking of record creation, updates, and deletions. This enhances data integrity and provides a clearer audit trail for user actions.

--- app/models/db_member.py
+++ app/models/db_member.py
@@ -1,7 +1,8 @@
 """Model Untuk Member / Concumer API / Otomax dan lain lain."""
 
-from sqlalchemy import Boolean, DateTime, Integer, String, func
+from sqlalchemy import Boolean, DateTime, String, func
 from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm.properties import MappedColumn
 
 from app.models import Base
 
@@ -11,29 +12,41 @@ class Member(Base):
 
     __tablename__ = "members"
 
-    id = mapped_column(
-        Integer, primary_key=True, index=True, nullable=False, autoincrement=True
+    memberid: MappedColumn[str] = mapped_column(
+        String(32), index=True, nullable=False, unique=True, primary_key=True
     )
-    memberid = mapped_column(String(32), index=True, nullable=False, unique=True)
-    name = mapped_column(String(100), nullable=False)
-    ipaddress = mapped_column(String(45), nullable=False)  # cukup utk IPv4/IPv6 teks
-    report_url = mapped_column(String(2048), nullable=False)  # URL panjang
-    pin = mapped_column(String(255), nullable=False)
-    password = mapped_column(String(255), nullable=False)
-    is_active = mapped_column(Boolean(), default=True, nullable=False)
-    allow_nosign = mapped_column(Boolean(), default=False, nullable=False)
-    created_at = mapped_column(
-        DateTime(timezone=True), server_default=func.now(), nullable=False
+    name: MappedColumn[str] = mapped_column(String(100), nullable=False)
+    ipaddress: MappedColumn[str] = mapped_column(
+        String(45), nullable=False
+    )  # cukup utk IPv4/IPv6 teks
+    report_url: MappedColumn[str] = mapped_column(
+        String(2048), nullable=False
+    )  # URL panjang
+    pin: MappedColumn[str] = mapped_column(String(255), nullable=False)
+    password: MappedColumn[str] = mapped_column(String(255), nullable=False)
+    allow_nosign: MappedColumn[bool] = mapped_column(
+        Boolean(), default=False, nullable=False
     )
-    updated_at = mapped_column(
-        DateTime(timezone=True),
-        server_default=func.now(),
-        onupdate=func.now(),
-        nullable=False,
+    is_active: MappedColumn[bool] = mapped_column(
+        Boolean(), default=True, nullable=False
+    )
+    created_at: MappedColumn[DateTime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
+    )
+
+    updated_at: MappedColumn[DateTime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
+    )
+
+    is_deleted: MappedColumn[bool] = mapped_column(
+        Boolean(), default=False, nullable=False
+    )
+    deleted_at: MappedColumn[DateTime] = mapped_column(
+        DateTime(timezone=True), default=None, nullable=True
     )
 
     def __repr__(self) -> str:
-        return f"<Member id={self.id} memberid={self.memberid} name={self.name}>"
+        return f"<Member memberid={self.memberid} name={self.name}>"
 
     def __str__(self) -> str:
-        return f"Member {self.name} (ID: {self.id})"
+        return f"Member {self.name} (ID: {self.memberid})"
--- app/models/db_user.py
+++ app/models/db_user.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Boolean, DateTime, Integer, String, func
 from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm.properties import MappedColumn
 
 from app.models import Base
 
@@ -9,21 +10,27 @@ class User(Base):
 
     __tablename__ = "users"
 
-    id = mapped_column(Integer, primary_key=True)
-    username = mapped_column(String, unique=True, nullable=False)
-    email = mapped_column(String, unique=True, nullable=False)
-    full_name = mapped_column(String, nullable=False)
-    hashed_password = mapped_column(String, nullable=False)
-    is_active = mapped_column(Boolean, default=True)
-    is_superuser = mapped_column(Boolean, default=False)
-    created_at = mapped_column(
-        DateTime(timezone=True), server_default=func.now(), nullable=False
+    id: MappedColumn[int] = mapped_column(Integer, primary_key=True)
+    username: MappedColumn[str] = mapped_column(String, unique=True, nullable=False)
+    email: MappedColumn[str] = mapped_column(String, unique=True, nullable=False)
+    full_name: MappedColumn[str] = mapped_column(String, nullable=False)
+    hashed_password: MappedColumn[str] = mapped_column(String, nullable=False)
+    is_active: MappedColumn[bool] = mapped_column(Boolean, default=True)
+    is_superuser: MappedColumn[bool] = mapped_column(Boolean, default=False)
+
+    created_at: MappedColumn[DateTime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
     )
-    updated_at = mapped_column(
-        DateTime(timezone=True),
-        server_default=func.now(),
-        onupdate=func.now(),
-        nullable=False,
+
+    updated_at: MappedColumn[DateTime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
+    )
+
+    is_deleted: MappedColumn[bool] = mapped_column(
+        Boolean(), default=False, nullable=False
+    )
+    deleted_at: MappedColumn[DateTime] = mapped_column(
+        DateTime(timezone=True), default=None, nullable=True
     )
 
     def __repr__(self) -> str:
